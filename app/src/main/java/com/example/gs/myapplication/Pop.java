cd64ec88acf88bc8193a50df4ac6539f20c576c2
gabrielsaimo <gabrielsaimo68@gmail.com>
2019-11-19 21:59:03 -0300

    mais leve ainda.

--- app/src/main/java/com/example/gs/myapplication/Pop.java
+++ app/src/main/java/com/example/gs/myapplication/Pop.java
@@ -25,13 +25,11 @@ public class Pop extends Activity {
     protected  void  onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.popwindow);
-
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
-
         int width = dm.widthPixels;
         int height = dm.heightPixels;
-        getWindow().setLayout((int)(width*.8),(int)(height*.6));
+        getWindow().setLayout((int)(width*.8),(int)(height*.3));
         inicializarComponentes();
         clicksBotoes();
     }
